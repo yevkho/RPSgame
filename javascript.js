16e22eb3c066880768fe9eb26fe3657708cdbeef
Yevgen Khodakovskyy <133250467+yevkho@users.noreply.github.com>
2024-05-06 09:30:37 -0300

    add UI to the game

--- javascript.js
+++ javascript.js
@@ -1,6 +1,5 @@
 function getComputerChoice () {
     const x = Math.random();
-    console.log(x);
     if (x < 0.33) {
         const y = "rock"
         return y
@@ -13,58 +12,48 @@ function getComputerChoice () {
     }
 }
 
-function getHumanChoice () {
-    let x = prompt("Type: 'rock' 'paper' or 'scissors'", "let's play");
-    x = x.toLowerCase();
-    console.log(x);
-    if (x === "rock" || x === "paper" || x === "scissors") {
-        return x
-    } else {
-        console.log("Wrong input")
-        x = getHumanChoice();
-        return x;
-        // not sure if the best, can't do a custom 'wrong input' message.
-    }    
-}
+let humanScore = 0;
+let computerScore = 0;
 
-function playGame() {
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+    button.addEventListener("click", playRound)
+})
 
-    let humanScore = 0;
-    let computerScore = 0;
-
-    function playRound(humanChoice, computerChoice) {
-        if (humanChoice === "rock" && computerChoice === "scissors") {
+    function playRound(e) {
+        
+        let playerSelection = e.target.id;
+        console.log(playerSelection);
+        
+        let computerSelection = getComputerChoice();
+        console.log(computerSelection);
+        
+        if (playerSelection === "rock" && computerSelection === "scissors") {
             console.log("You win! Paper beats Rock");
             humanScore += 1;
-        } else if (humanChoice === "scissors" && computerChoice === "paper") {
+        } else if (playerSelection === "scissors" && computerSelection === "paper") {
             console.log("You win! Scissors beats Paper");
             humanScore += 1;
-        } else if (humanChoice === "paper" && computerChoice === "rock") {
+        } else if (playerSelection === "paper" && computerSelection === "rock") {
             console.log("You win! Paper beats Rock");
             humanScore += 1;
-        } else if (humanChoice === computerChoice) {
+        } else if (playerSelection === computerSelection) {
             console.log("It's a tie!");
         } else {
             console.log("You loose!");
             computerScore += 1;
             // could *3 to match human, but there may be a better solution.
         }
-        console.log(`You get ${humanScore}, Computer gets ${computerScore}`)
+        const result = document.querySelector("#result");
+        result.textContent = `You get ${humanScore}, Computer gets ${computerScore}`;
+
+        if (humanScore === 5) {
+            result.textContent = "You Won the Game!";
+            humanScore = 0;
+            computerScore = 0;
+        } else if (computerScore === 5) {
+            result.textContent = "You Lost the Game...";
+            humanScore = 0;
+            computerScore = 0;
+        }
     } 
-
-    for (let i = 0; i < 5; i++) {
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-        playRound(humanSelection, computerSelection);
-    }
-
-    if (humanScore > computerScore) {
-        console.log("You Won the Game!")
-    } else if (humanScore < computerScore) {
-        console.log("You Lost the Game...")
-    } else {
-        console.log("It's a tie.")
-    }
-}
-
-playGame()
