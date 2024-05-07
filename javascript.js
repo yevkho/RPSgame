2dc4a0dc2648bbcb9e5ced60a721f6a4dbeae4f2
Yevgen Khodakovskyy <133250467+yevkho@users.noreply.github.com>
2024-05-07 06:47:49 -0300

    final tweaks

--- javascript.js
+++ javascript.js
@@ -1,59 +1,55 @@
-function getComputerChoice () {
-    const x = Math.random();
-    if (x < 0.33) {
-        const y = "rock"
-        return y
-    } else if (x >= 0.33 && x < 0.66) {
-        const y = "paper"
-        return y
-    } else {
-        const y = "scissors"
-        return y
-    }
-}
 
 let humanScore = 0;
 let computerScore = 0;
+
+const endResult = document.querySelector("#endResult");
 
 const buttons = document.querySelectorAll("button");
 buttons.forEach((button) => {
     button.addEventListener("click", playRound)
 })
 
-    function playRound(e) {
-        
-        let playerSelection = e.target.id;
-        console.log(playerSelection);
-        
-        let computerSelection = getComputerChoice();
-        console.log(computerSelection);
-        
-        if (playerSelection === "rock" && computerSelection === "scissors") {
-            console.log("You win! Paper beats Rock");
-            humanScore += 1;
-        } else if (playerSelection === "scissors" && computerSelection === "paper") {
-            console.log("You win! Scissors beats Paper");
-            humanScore += 1;
-        } else if (playerSelection === "paper" && computerSelection === "rock") {
-            console.log("You win! Paper beats Rock");
-            humanScore += 1;
-        } else if (playerSelection === computerSelection) {
-            console.log("It's a tie!");
-        } else {
-            console.log("You loose!");
-            computerScore += 1;
-            // could *3 to match human, but there may be a better solution.
-        }
-        const result = document.querySelector("#result");
-        result.textContent = `You get ${humanScore}, Computer gets ${computerScore}`;
+let selections = ["rock", "paper", "scissors"];
+function getComputerChoice () {
+    return selections[Math.floor(Math.random()*3)]
+}
 
+function endGame () {
         if (humanScore === 5) {
-            result.textContent = "You Won the Game!";
-            humanScore = 0;
-            computerScore = 0;
-        } else if (computerScore === 5) {
-            result.textContent = "You Lost the Game...";
-            humanScore = 0;
-            computerScore = 0;
-        }
+        endResult.textContent = "You Won the Game!";
+        humanScore = 0;
+        computerScore = 0;
+    } else if (computerScore === 5) {
+        endResult.textContent = "You Lost the Game...";
+        humanScore = 0;
+        computerScore = 0;
+    }
+}
+
+function playRound(e) {   
+    let playerSelection = e.target.id;
+    let computerSelection = getComputerChoice(); 
+    const currentResult = document.querySelector("#currentResult");
+    
+    if (playerSelection === "rock" && computerSelection === "scissors") {
+        currentResult.textContent = "You win! Paper beats Rock";
+        humanScore += 1;
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        currentResult.textContent = "You win! Scissors beats Paper";
+        humanScore += 1;
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        currentResult.textContent = "You win! Paper beats Rock";
+        humanScore += 1;
+    } else if (playerSelection === computerSelection) {
+        currentResult.textContent = "It's a tie!";
+    } else {
+        currentResult.textContent = "You loose!";
+        computerScore += 1;
     } 
+    
+    endResult.textContent = `You get ${humanScore}, Computer gets ${computerScore}`;
+    
+    if (humanScore === 5 || computerScore ===5) {
+        endGame()
+    }
+} 
